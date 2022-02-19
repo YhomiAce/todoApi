2769ef3ec3c7167b9a3ff383ddf2a53c9a8adc7a
YhomiAce <kareemyomi91@gmail.com>
2022-02-19 22:24:22 +0100

    add seeders

---
+++ database/seeders/TodoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->count(10)->create();
+        Todo::factory()->count(100)->create();
+    }
+}
